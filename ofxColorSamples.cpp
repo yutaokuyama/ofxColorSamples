2748646f4d99225af24215eb45c5c7d05aeac82a
yutaokuyama <yutaokuyam@gmail.com>
2019-09-21 09:04:52 +0900

    add files

---
+++ ofxColorSamples.cpp
@@ -0,0 +1,9 @@
+//
+//  ofxColorSamples.cpp
+//  instancing_allow
+//
+//  Created by 奥山裕大 on 9/21/31 H.
+//
+
+#include "ofxColorSamples.h"
+
---
+++ ofxColorSamples.h
@@ -0,0 +1,27 @@
+#pragma  once
+
+
+#include "ofMain.h"
+
+
+class ofxColorSamples{
+public:
+    ofFloatColor blues[10];
+    
+    
+    void setup(){
+    blues[0] = ofFloatColor(33.0/255.0,55.0/255.0,77.0/255.0,255.0/255.0);
+    blues[1] = ofFloatColor(33.0/255.0,55.0/255.0,77.0/255.0,255.0/255.0);
+    blues[2] = ofFloatColor(33.0/255.0,55.0/255.0,77.0/255.0,255.0/255.0);
+    blues[3] = ofFloatColor(33.0/255.0,55.0/255.0,77.0/255.0,255.0/255.0);
+    blues[4] = ofFloatColor(16.0/255.0,137.0/255.0,255/255.0,255);
+    blues[5] = ofFloatColor(16.0/255.0,137.0/255.0,255.0/255.0,255.0/255.0);
+    blues[6] = ofFloatColor(16.0/255.0,137.0/255.0,255.0/255.0,255.0/255.0);
+    blues[7] =  ofFloatColor(229.0/255.0,229.0/255.0,229.0/255.0,255.0/255.0);
+    blues[8] = ofFloatColor(229.0/255.0,229.0/255.0,229./255.0,255.0/255.0);
+    blues[9] = ofFloatColor(238.0/255.0,238.0/255.0,238/255.0,255.0/255.0);
+    }
+    
+    
+    
+};
